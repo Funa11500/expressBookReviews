1ef4a815c4c1945d7a165891fefd3e5567bec9d1
Juan Higuera <juanhiguera204@gmail.com>
2023-06-11 12:02:29 -0500

    Fixing some bugs.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,40 +29,64 @@ public_users.get('/', async function (req, res) {
   await res.send(JSON.stringify(books, null, 5))
 });
 
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
-  if(req.params.isbn>=1 && req.params.isbn<=10){
-    await res.send(books[req.params.isbn])
-  }
-  await res.send('Book not Available')
+public_users.get('/isbn/:isbn', function(req, res) {
+  return new Promise(function(resolve, reject) {
+    if (req.params.isbn != null && req.params.isbn >= 1 && req.params.isbn <= 10) {
+      resolve(books[req.params.isbn]);
+    } else {
+      reject('Book not Available');
+    }
+  })
+  .then(
+    function(result) {
+    res.send(result);
+  }, 
+    function(err){
+      res.send(err)
+    }
+  )
 });
   
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
   let author = req.params.author;
   if(author){
+    let flag = true;
     for(let i=0; i<=9 ;i++){
       if(Object.values(books)[i].author ==  req.params.author){
+        flag = false
         await res.send(books[i+1])
       }
     }
-    await res.send('Author not Found!')
+    if(flag){
+      await res.send('Author not Found!')
+    }
   }
-  await res.send("Author Not Provided!")
+  else{
+    await res.send("Author Not Provided!")
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
   let title = req.params.title
   if(title){
+    let flag = true;
     for(let i=0; i<=9 ;i++){
       if(Object.values(books)[i].title ==  req.params.title){
+        flag = false
         await res.send(books[i+1])
       }
     }
-    await res.send('Title not Found!')
+    if(flag){
+      await res.send('Title not Found!')
+    }
   }
-  await res.send("Title Not Provided!")
+  else{
+    await res.send("Title Not Provided!")
+  }
 });
 
 //  Get book review
@@ -72,9 +96,13 @@ public_users.get('/review/:isbn', async function (req, res) {
     if(isbn>=1 && isbn<=10){
       await res.send(books[isbn].reviews)
     }
-    await res.send("ISBN Not Valid!")
+    else{
+      await res.send("ISBN Not Valid!")
+    }
   }
-  await res.send("ISBN Not Provided!")
+  else{
+    await res.send("ISBN Not Provided!")
+  }
 });
 
 module.exports.general = public_users;
